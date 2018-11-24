b9a962906194597fa0ec625da9ec1c46164b2918
Patrick Ball <slaapz@gmail.com>
2018-11-23 22:45:34 -0700

    Added connection to mlab server

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// connect to mlab database
+mongoose.connect('mongodb://pat:test123@ds115664.mlab.com:15664/gql-server');
+mongoose.connection.once('open', () => {
+  console.log('Connected to the database');
+});
 
 // This middleware get passed the schema
 app.use(
