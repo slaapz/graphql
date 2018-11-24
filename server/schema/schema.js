efebb04019987251c62633110068ff4c21c34ac7
Patrick Ball <slaapz@gmail.com>
2018-11-23 21:37:03 -0700

    Added the RootQuery to the schema, then the schema to the app.js file

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,8 @@
 const graphql = requre('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// Types
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -9,4 +10,22 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        //  Code to get data from db/other source
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
