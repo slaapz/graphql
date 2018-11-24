a5df1337a20dbc1cfedba5af59dd0612acd1f446
Patrick Ball <slaapz@gmail.com>
2018-11-23 21:18:35 -0700

    Added graphql

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
+
+// This middleware get passed the schema
+app.use('/graphql', graphqlHTTP({}));
 
 app.listen(4000, () => {
   console.log(`Server started on port 4000`);
