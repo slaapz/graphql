3d2d51424f9c4918dfb5439375f99980b580f16c
Patrick Ball <slaapz@gmail.com>
2018-11-26 21:10:23 -0700

    Added refetchQueries method to the submitForm method of the AddBook component.

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { graphql, compose } from 'react-apollo';
-import { getAuthorsQuery, addBookMutation } from '../queries/queries';
+import {
+  getAuthorsQuery,
+  addBookMutation,
+  getBooksQuery
+} from '../queries/queries';
 
 // Queries
 
@@ -35,7 +39,8 @@ class AddBook extends Component {
         name: this.state.name,
         genre: this.state.genre,
         authorId: this.state.authorId
-      }
+      },
+      refetchQueries: [{ query: getBooksQuery }]
     });
   }
   render() {
